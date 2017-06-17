7e7da7f1f9355c3839ae558c918eb8adc23ff3d5
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-06-17 17:11:53 -0400

    Color table showed up

---
+++ comments/tableSkinColor.php
@@ -0,0 +1,46 @@
+<table class="tg">
+  <tr>
+    <th class="tg-yw4l" id="white" style="background-color:white;"></th>
+    <th class="tg-yw4l" id="antiquewhite" style="background-color:antiquewhite;"></th>
+    <th class="tg-yw4l" id="papayawhip" style="background-color:papayawhip;"></th>
+    <th class="tg-yw4l" id="navajowhite" style="background-color:navajowhite;"></th>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" id="peachpuff" style="background-color:peachpuff;"></td>
+    <td class="tg-yw4l" id="lightpink" style="background-color:lightpink;"></td>
+    <td class="tg-yw4l" id="pink" style="background-color:pink;"></td>
+    <td class="tg-yw4l" id="moccasin" style="background-color:moccasin;"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" id="beige" style="background-color:beige;"></td>
+    <td class="tg-yw4l" id="khaki" style="background-color:khaki;"></td>
+    <td class="tg-yw4l" id="tan" style="background-color:tan;"></td>
+    <td class="tg-yw4l" id="salmon" style="background-color:salmon;"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" id="peru" style="background-color:peru;"></td>
+    <td class="tg-yw4l" id="goldenrod" style="background-color:goldenrod;"></td>
+    <td class="tg-yw4l" id="darksalmon" style="background-color:darksalmon;"></td>
+    <td class="tg-yw4l" id="indianred" style="background-color:indianred;"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" id="red"style="background-color:red;" ></td>
+    <td class="tg-yw4l" id="darkred" style="background-color:darkred;"></td>
+    <td class="tg-yw4l" id="chocolate" style="background-color:chocolate;"></td>
+    <td class="tg-yw4l" id="brown" style="background-color:brown;"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" id="black" style="background-color:black;"></td>
+    <td class="tg-yw4l" id="mediumslateblue" style="background-color:mediumslateblue;"></td>
+    <td class="tg-yw4l" id="purple" style="background-color:purple;"></td>
+    <td class="tg-yw4l" id="lavender" style="background-color:lavender;"></td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l" id="blue" style="background-color:blue;"></td>
+    <td class="tg-yw4l" id="green" style="background-color:green;"></td>
+    <td class="tg-yw4l" id="yellow" style="background-color:yellow;"></td>
+    <td class="tg-yw4l" id="orange" style="background-color:orange;"></td>
+  </tr>
+</table>
+<br>
+Choose a skin color!
