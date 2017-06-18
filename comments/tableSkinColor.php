ce8c52ed4e45d1bd9d02d03f5f51eb1b8b2a4bf0
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-06-17 21:36:34 -0400

    Used color picker to change human skin

--- comments/tableSkinColor.php
+++ comments/tableSkinColor.php
@@ -1,46 +1,38 @@
-<table class="tg">
-  <tr>
-    <th class="tg-yw4l" id="white" style="background-color:white;"></th>
-    <th class="tg-yw4l" id="antiquewhite" style="background-color:antiquewhite;"></th>
-    <th class="tg-yw4l" id="papayawhip" style="background-color:papayawhip;"></th>
-    <th class="tg-yw4l" id="navajowhite" style="background-color:navajowhite;"></th>
-  </tr>
-  <tr>
-    <td class="tg-yw4l" id="peachpuff" style="background-color:peachpuff;"></td>
-    <td class="tg-yw4l" id="lightpink" style="background-color:lightpink;"></td>
-    <td class="tg-yw4l" id="pink" style="background-color:pink;"></td>
-    <td class="tg-yw4l" id="moccasin" style="background-color:moccasin;"></td>
-  </tr>
-  <tr>
-    <td class="tg-yw4l" id="beige" style="background-color:beige;"></td>
-    <td class="tg-yw4l" id="khaki" style="background-color:khaki;"></td>
-    <td class="tg-yw4l" id="tan" style="background-color:tan;"></td>
-    <td class="tg-yw4l" id="salmon" style="background-color:salmon;"></td>
-  </tr>
-  <tr>
-    <td class="tg-yw4l" id="peru" style="background-color:peru;"></td>
-    <td class="tg-yw4l" id="goldenrod" style="background-color:goldenrod;"></td>
-    <td class="tg-yw4l" id="darksalmon" style="background-color:darksalmon;"></td>
-    <td class="tg-yw4l" id="indianred" style="background-color:indianred;"></td>
-  </tr>
-  <tr>
-    <td class="tg-yw4l" id="red"style="background-color:red;" ></td>
-    <td class="tg-yw4l" id="darkred" style="background-color:darkred;"></td>
-    <td class="tg-yw4l" id="chocolate" style="background-color:chocolate;"></td>
-    <td class="tg-yw4l" id="brown" style="background-color:brown;"></td>
-  </tr>
-  <tr>
-    <td class="tg-yw4l" id="black" style="background-color:black;"></td>
-    <td class="tg-yw4l" id="mediumslateblue" style="background-color:mediumslateblue;"></td>
-    <td class="tg-yw4l" id="purple" style="background-color:purple;"></td>
-    <td class="tg-yw4l" id="lavender" style="background-color:lavender;"></td>
-  </tr>
-  <tr>
-    <td class="tg-yw4l" id="blue" style="background-color:blue;"></td>
-    <td class="tg-yw4l" id="green" style="background-color:green;"></td>
-    <td class="tg-yw4l" id="yellow" style="background-color:yellow;"></td>
-    <td class="tg-yw4l" id="orange" style="background-color:orange;"></td>
-  </tr>
-</table>
-<br>
+<div id="colorButtons">
+	  <tr>
+    <button id="white" style="background-color:white;">White</button>
+    <button id="antiquewhite" style="background-color:antiquewhite;">AntiqueWhite</button>
+    <button id="papayawhip" style="background-color:papayawhip;">Papayawhip</button>
+    <button id="navajowhite" style="background-color:navajowhite;">NavajoWhite</button>
+	<br>
+    <button id="peachpuff" style="background-color:peachpuff;">PeachPuff</button>
+    <button id="lightpink" style="background-color:lightpink;">Light Pink</button>
+    <button id="pink" style="background-color:pink;">Pink</button>
+    <button id="moccasin" style="background-color:moccasin;">Moccasin</button>
+	<br>
+    <button id="beige" style="background-color:beige;">Beige</button>
+    <button id="khaki" style="background-color:khaki;">Khaki</button>
+    <button id="tan" style="background-color:tan;">Tan</button>
+    <button id="salmon" style="background-color:salmon;">Salmon</button>
+	<br>
+    <button id="peru" style="background-color:peru;">Peru</button>
+    <button id="goldenrod" style="background-color:goldenrod;">Goldenrod</button>
+    <button id="darksalmon" style="background-color:darksalmon;">Dark Salmon</button>
+    <button id="indianred" style="background-color:indianred;">IndianRed</button>
+    <br>
+	<button id="red" style="background-color:red;">Red</button>
+    <button id="darkred">DarkRed</button>
+    <button id="chocolate" style="background-color:chocolate;">Chocolate</button>
+    <button id="brown" style="background-color:brown;">Brown</button>
+    <br>
+	<button id="black" style="background-color:black;">Black</button>
+    <button id="mediumslateblue" style="background-color:mediumslateblue;">Gray-Blue</button>
+    <button id="purple" style="background-color:purple;">Purple</button>
+    <button id="lavender" style="background-color:lavender;">Lavender</button>
+	<br>
+    <button id="blue" style="background-color:blue;">Blue</button>
+    <button id="green" style="background-color:green;">Green</button>
+    <button id="yellow" style="background-color:yellow;">Yellow</button>
+    <button id="orange" style="background-color:orange;">Orange</button>
+</div>
 Choose a skin color!
--- human.php
+++ human.php
@@ -53,6 +53,8 @@
 	<script src="js/jquery.js"></script>
 	<script src="js/jquery-ui.js"></script>
 	<script src="js/touch_punch.js"></script>
+	<script src="js/jquery.colorPicker.js"></script>
+	<link rel="stylesheet" href="css/colorPicker.css" type="text/css" />
 	<title>Erintuitive's Psychic Place</title>
 </head>
 <body>
@@ -152,8 +154,15 @@
 		}
 		function nextOptions() {
 			if(avatarOptions.getAttribute("class")=="init"){
-				itemPreview.innerHTML = '<table class="tg"> <tr> <th class="tg-yw4l" id="white" style="background-color:white;"></th> <th class="tg-yw4l" id="antiquewhite" style="background-color:antiquewhite;"></th> <th class="tg-yw4l" id="papayawhip" style="background-color:papayawhip;"></th> <th class="tg-yw4l" id="navajowhite" style="background-color:navajowhite;"></th> </tr> <tr> <td class="tg-yw4l" id="peachpuff" style="background-color:peachpuff;"></td> <td class="tg-yw4l" id="lightpink" style="background-color:lightpink;"></td> <td class="tg-yw4l" id="pink" style="background-color:pink;"></td> <td class="tg-yw4l" id="moccasin" style="background-color:moccasin;"></td> </tr> <tr> <td class="tg-yw4l" id="beige" style="background-color:beige;"></td> <td class="tg-yw4l" id="khaki" style="background-color:khaki;"></td> <td class="tg-yw4l" id="tan" style="background-color:tan;"></td> <td class="tg-yw4l" id="salmon" style="background-color:salmon;"></td> </tr> <tr> <td class="tg-yw4l" id="peru" style="background-color:peru;"></td> <td class="tg-yw4l" id="goldenrod" style="background-color:goldenrod;"></td> <td class="tg-yw4l" id="darksalmon" style="background-color:darksalmon;"></td> <td class="tg-yw4l" id="indianred" style="background-color:indianred;"></td> </tr> <tr> <td class="tg-yw4l" id="red"style="background-color:red;" ></td> <td class="tg-yw4l" id="darkred" style="background-color:darkred;"></td> <td class="tg-yw4l" id="chocolate" style="background-color:chocolate;"></td> <td class="tg-yw4l" id="brown" style="background-color:brown;"></td> </tr> <tr> <td class="tg-yw4l" id="black" style="background-color:black;"></td> <td class="tg-yw4l" id="mediumslateblue" style="background-color:mediumslateblue;"></td> <td class="tg-yw4l" id="purple" style="background-color:purple;"></td> <td class="tg-yw4l" id="lavender" style="background-color:lavender;"></td> </tr> <tr> <td class="tg-yw4l" id="blue" style="background-color:blue;"></td> <td class="tg-yw4l" id="green" style="background-color:green;"></td> <td class="tg-yw4l" id="yellow" style="background-color:yellow;"></td> <td class="tg-yw4l" id="orange" style="background-color:orange;"></td> </tr></table><br><div id="chooseSkin">Choose a skin color!</div>';
+				itemPreview.innerHTML = '<div><label for="color1">Color 1</label> <input id="color1" type="text" name="color1" value="#333399" onchange="makeSkinColor()"/></div>';
+				$('#color1').colorPicker();
 				avatarOptions.setAttribute("class", "skinStep");
+			}
+		}
+		function makeSkinColor() {
+			for (i=0; i<skins.length; i++){
+				var updateSkin = document.getElementById("color1").getAttribute("value");
+				skins[i].setAttribute("fill", updateSkin);
 			}
 		}
 		function man() {
