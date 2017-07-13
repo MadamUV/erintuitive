c89d51eea5d4f17d87713e28ae77f321af28c174
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-07-13 17:53:41 -0400

    maybe it is a bug with the post vars

--- addHuman.php
+++ addHuman.php
@@ -1,13 +1,12 @@
 <?php
 
 include 'config.php';
-
+	$sql = "INSERT INTO `humans` (`id`, `me_id`, `avatar`) VALUES (NULL, '1211212213213', '<div id='div'></div>');";
+	$mysqli->query($sql);
 if(isset($_POST['me_id']) && isset($_POST['avatar'])) {
 	$me_id = $_POST['me_id'];
 	$avatar = $_POST['avatar'];
 	//get data
-	$sql = "INSERT INTO `humans` (`id`, `me_id`, `avatar`) VALUES (NULL, '1211212213213', '<div id='div'></div>');";
-	$mysqli->query($sql);
 	if($result = $mysqli->query("SELECT * FROM humans WHERE me_id='" . $me_id . "' LIMIT 1")){
 		$row_count = $result->num_rows;
 		if ($row_count == 0){
