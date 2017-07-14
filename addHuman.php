54d5872851970e39431030d328d8b6b19bfee097
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-07-14 00:03:59 -0400

    updated sql for test

--- addHuman.php
+++ addHuman.php
@@ -1,20 +1,20 @@
 <?php
 
 include 'config.php';
-	$sql = "INSERT INTO `humans` (`id`, `me_id`, `avatar`) VALUES (NULL, '1211212213213', '<div id='div'></div>');";
-	$mysqli->query($sql);
+	//$sql = "INSERT INTO `humans` (`id`, `me_id`, `avatar`) VALUES (NULL, '1211212213213', '<div id='div'></div>');";
+	//$mysqli->query($sql);
 if(isset($_POST['me_id']) && isset($_POST['avatar'])) {
 	$me_id = $_POST['me_id'];
 	$avatar = $_POST['avatar'];
-	//get data
-	if($result = $mysqli->query("SELECT * FROM humans WHERE me_id='" . $me_id . "' LIMIT 1")){
+	//get data 
+	if($result = $mysqli->query("SELECT * FROM `humans` WHERE `me_id`='". $me_id . "' LIMIT 1;")){
 		$row_count = $result->num_rows;
 		if ($row_count == 0){
-			$sql = "INSERT INTO humans (me_id, avatar) VALUES ('" . $me_id . "', '" . $avatar . "')";
+			$sql = "INSERT INTO `humans` (`id`, `me_id`, `avatar`) VALUES (NULL, '".$me_id."', '".$avatar."');";
 			$mysqli->query($sql);
 		}
 		else {
-			$sql = "UPDATE humans SET me_id='".$me_id."', avatar='".$avatar."'";
+			$sql = "UPDATE `humans` SET `avatar`='".$me_id."' WHERE `me_id`='".$avatar."';";
 			$mysqli->query($sql);
 		}
 	}
