eb89cba6e7b57ac3e8c445b3e4215f994ca456e7
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-07-18 07:34:57 -0400

    check if push is the problem

--- humanClothes.php
+++ humanClothes.php
@@ -97,8 +97,9 @@
 								"pos_y":-1
 							};
 							data3['person'].push(pushThis);
-							//var len = data3['person'].length;
-							$.ajax({
+							var len = data3['person'].length;
+							document.getElementById("relativeContainer").innerHTML = "ok";
+							/*$.ajax({
 								url:"https://api.myjson.com/bins/vzecj",
 								type:"PUT",
 								data: data3,
@@ -107,7 +108,7 @@
 								success: function(data, textStatus, jqXHR){
 									document.getElementById("relativeContainer").innerHTML = "ok";
 								}
-							});
+							});*/
 						}
 						else if(count1 > 0 && count2 > 0){
 							data3['person'][countPresent]['avatar'] = "<? echo $avatar; ?>";
