ecebd7145debe3b158600a9e4fe62f0b5e927122
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-07-29 03:00:38 -0400

    moved script

--- humanClothes.php
+++ humanClothes.php
@@ -56,91 +56,6 @@
 					//10155567524149846 my user id
 				} ?>
 				</div>
-				<script>
-					var me_id = "<? echo $me_id; ?>";
-					var count1 = 0;
-					var count2 = 0;
-					var countPresent = 0;
-					$.get("https://api.myjson.com/bins/vzecj", function (data3, textStatus3, jqXHR3) {
-						for(i=0; i<data3['person'].length; i++){
-							if(data3['person'][i]['user_id'] != me_id){
-								count1 += 1;
-							}
-							else {
-								count2 += 1;
-								countPresent = i;
-							}
-						}
-						if(count1 == 0 && count2 > 0){
-							$.ajax({
-								url:"https://api.myjson.com/bins/vzecj",
-								type:"PUT",
-								data:'{"person": [{"user_id":"<? echo $me_id; ?>", "name":"guest", "avatar":"<? echo $avatar; ?>", "pos_x":-1, "pos_y":-1}]}',
-								contentType:"application/json; charset=utf-8",
-								dataType:"json",
-								success: function(data, textStatus, jqXHR){
-									$.get("https://api.myjson.com/bins/vzecj", function (data, textStatus, jqXHR) {
-										var avatarJSON = data['person'][0]['avatar'];
-										$.post("convertAvatar.php", {convert: avatarJSON}, function(data2){
-											document.getElementById("relativeContainer").innerHTML = data2;
-										});
-									});
-								}
-							});
-						}
-						else if(count1 > 0 && count2 == 0){
-							var pushThis = {
-								"user_id": me_id,
-								"name":"guest",
-								"avatar":"<? echo $avatar; ?>",
-								"pos_x":-1,
-								"pos_y":-1
-							};
-							data3['person'].push(pushThis);
-							var len = data3['person'].length;
-							document.getElementById("relativeContainer").innerHTML = "ok";
-							$.ajax({
-								url:"https://api.myjson.com/bins/vzecj",
-								type:"PUT",
-								data: JSON.stringify(data3),
-								contentType:"application/json; charset=utf-8",
-								dataType:"json",
-								success: function(data, textStatus, jqXHR){
-									$.get("https://api.myjson.com/bins/vzecj", function (data, textStatus, jqXHR) {
-										var avatarJSON = data['person'][len-1]['avatar'];
-										$.post("convertAvatar.php", {convert: avatarJSON}, function(data2){
-											document.getElementById("relativeContainer").innerHTML = data2;
-										});
-									});
-								}
-							});
-						}
-						else if(count1 > 0 && count2 > 0){
-							for(i=0; i<data3['person'].length; i++){
-								if(data3['person'][i]['user_id']==me_id){
-									data3['person'][i]['avatar'] = "<? echo $avatar; ?>";
-									$.ajax({
-										url:"https://api.myjson.com/bins/vzecj",
-										type:"PUT",
-										data: JSON.stringify(data3),
-										contentType:"application/json; charset=utf-8",
-										dataType:"json",
-										success: function(data, textStatus, jqXHR){
-											$.get("https://api.myjson.com/bins/vzecj", function (data, textStatus, jqXHR) {
-												var avatarJSON = data['person'][i]['avatar'];
-												$.post("convertAvatar.php", {convert: avatarJSON}, function(data2){
-													document.getElementById("relativeContainer").innerHTML = data2;
-												});
-											});
-										}
-									});
-									break;
-								}
-							}
-							
-						}
-					});
-				</script>
 			</td>
 		</tr>
 	</table>
@@ -148,6 +63,91 @@
 		<button id="back" onclick="backOptions()">Back</button>
 		<button id="next" onclick="nextOptions()">Next</button>
 	</div>
+	<script>
+		var me_id = "<? echo $me_id; ?>";
+		var count1 = 0;
+		var count2 = 0;
+		var countPresent = 0;
+		$.get("https://api.myjson.com/bins/vzecj", function (data3, textStatus3, jqXHR3) {
+			for(i=0; i<data3['person'].length; i++){
+				if(data3['person'][i]['user_id'] != me_id){
+					count1 += 1;
+				}
+				else {
+					count2 += 1;
+					countPresent = i;
+				}
+			}
+			if(count1 == 0 && count2 > 0){
+				$.ajax({
+					url:"https://api.myjson.com/bins/vzecj",
+					type:"PUT",
+					data:'{"person": [{"user_id":"<? echo $me_id; ?>", "name":"guest", "avatar":"<? echo $avatar; ?>", "pos_x":-1, "pos_y":-1}]}',
+					contentType:"application/json; charset=utf-8",
+					dataType:"json",
+					success: function(data, textStatus, jqXHR){
+						$.get("https://api.myjson.com/bins/vzecj", function (data, textStatus, jqXHR) {
+							var avatarJSON = data['person'][0]['avatar'];
+							$.post("convertAvatar.php", {convert: avatarJSON}, function(data2){
+								document.getElementById("relativeContainer").innerHTML = data2;
+							});
+						});
+					}
+				});
+			}
+			else if(count1 > 0 && count2 == 0){
+				var pushThis = {
+					"user_id": me_id,
+					"name":"guest",
+					"avatar":"<? echo $avatar; ?>",
+					"pos_x":-1,
+					"pos_y":-1
+				};
+				data3['person'].push(pushThis);
+				var len = data3['person'].length;
+				document.getElementById("relativeContainer").innerHTML = "ok";
+				$.ajax({
+					url:"https://api.myjson.com/bins/vzecj",
+					type:"PUT",
+					data: JSON.stringify(data3),
+					contentType:"application/json; charset=utf-8",
+					dataType:"json",
+					success: function(data, textStatus, jqXHR){
+						$.get("https://api.myjson.com/bins/vzecj", function (data, textStatus, jqXHR) {
+							var avatarJSON = data['person'][len-1]['avatar'];
+							$.post("convertAvatar.php", {convert: avatarJSON}, function(data2){
+								document.getElementById("relativeContainer").innerHTML = data2;
+							});
+						});
+					}
+				});
+			}
+			else if(count1 > 0 && count2 > 0){
+				for(i=0; i<data3['person'].length; i++){
+					if(data3['person'][i]['user_id']==me_id){
+						data3['person'][i]['avatar'] = "<? echo $avatar; ?>";
+						$.ajax({
+							url:"https://api.myjson.com/bins/vzecj",
+							type:"PUT",
+							data: JSON.stringify(data3),
+							contentType:"application/json; charset=utf-8",
+							dataType:"json",
+							success: function(data, textStatus, jqXHR){
+								$.get("https://api.myjson.com/bins/vzecj", function (data, textStatus, jqXHR) {
+									var avatarJSON = data['person'][i]['avatar'];
+									$.post("convertAvatar.php", {convert: avatarJSON}, function(data2){
+										document.getElementById("relativeContainer").innerHTML = data2;
+									});
+								});
+							}
+						});
+						break;
+					}
+				}
+				
+			}
+		});
+	</script>
 	<style>
 		body {
 			background-color: #FFBB22;
