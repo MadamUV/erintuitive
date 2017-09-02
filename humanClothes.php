168b5bc76ef84f8b19e7b2d47504d7f180154ed4
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-09-01 20:50:10 -0400

    40 index z

--- humanClothes.php
+++ humanClothes.php
@@ -305,7 +305,7 @@
 	function randomizeGlasses(){
 		$(".glasses").remove();
 		$("#relativeContainer").append(glasses()[0]);
-		$("#relativeContainer .glasses").css({'position':'absolute', 'top':'0', 'left':'0', 'margin-top':'0'});
+		$("#relativeContainer .glasses").css({'position':'absolute', 'z-index':'40', 'top':'0', 'left':'0', 'margin-top':'0'});
 	}
 	function randomizeAccessories(){
 		$(".accessories").remove();
