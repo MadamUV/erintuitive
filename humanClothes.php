2d782738f43d5cf515077f777b5280489a0476c6
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-09-10 01:25:25 -0400

    turned more getAttribute calls into aattr

--- humanClothes.php
+++ humanClothes.php
@@ -129,10 +129,10 @@
 		if($("#relativeContainer .pets")[0].attr("src")=="svg/human/humanClothes/pets/pet15.svg"){
 			$("#relativeContainer .pets").css({'top':'-5px'});
 		}
-		else if($("#relativeContainer .pets")[0].getAttribute("src")=="svg/human/humanClothes/pets/pet16.svg" || $("#relativeContainer .pets")[0].getAttribute("src")=="svg/human/humanClothes/pets/pet17.svg" || $("#relativeContainer .pets")[0].getAttribute("src")=="svg/human/humanClothes/pets/pet18.svg"){
+		else if($("#relativeContainer .pets")[0].attr("src")=="svg/human/humanClothes/pets/pet16.svg" || $("#relativeContainer .pets")[0].attr("src")=="svg/human/humanClothes/pets/pet17.svg" || $("#relativeContainer .pets")[0].attr("src")=="svg/human/humanClothes/pets/pet18.svg"){
 			$("#relativeContainer .pets").css({'top':'-15px'});
 		}
-		else if($("#relativeContainer .pets")[0].getAttribute("src")=="svg/human/humanClothes/pets/pet3.svg"){
+		else if($("#relativeContainer .pets")[0].attr("src")=="svg/human/humanClothes/pets/pet3.svg"){
 			$("#relativeContainer .pets").css({'top':'-9px', '-webkit-transform':'scale(1.2)', '-ms-transform':'scale(1.2)', 'transform':'scale(1.2)'});
 		}
 	}
