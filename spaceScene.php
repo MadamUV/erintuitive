aa96de044af690c963934c0cbfc99e5bb4209859
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-09-16 12:25:54 -0400

    removed width

--- spaceScene.php
+++ spaceScene.php
@@ -26,7 +26,7 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 </script>
-<table height="400px" id="saturn" style="background-color: black; position: absolute; z-index:200; left: 20px;" cellpadding="100px">
+<table id="saturn" style="background-color: black; position: absolute; z-index:200; left: 20px;" cellpadding="100px">
 	<tr><td>
 		<img src="img/stars5.gif" alt="saturn"/>
 	</td></tr>
