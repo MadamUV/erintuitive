5a413e3bb8cbe3f408c981badf0b9036bc3b7f2c
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-09-16 02:37:05 -0400

    got rid of cloud's syntax err

--- spaceScene.php
+++ spaceScene.php
@@ -125,7 +125,7 @@
 						var name = data3['person'][i]['name'];
 						$.post("convertAvatar.php", {convert: avatar}, function(data2){
 							avatar = data2;
-							$("#people").append('<div class="wrapperPeople" id="'+me_id+'" style="-webkit-transform: scale(1, 1); -ms-transform: scale(1, 1); transform: scale(1, 1); position: relative;"><div id="'+me_id+'_sub" class="person" style="position: absolute; top: 0; left: 0; z-index: 203; -webkit-transform: scale(0.54); -ms-transform: scale(0.54); transform: scale(0.54);">'+avatar+'<img style="position: absolute; top: 168px; left:40px: -130px; z-index: 202;" width="200%" src="svg/human/humanBody/cloud.svg"/><span style="position: absolute; top: 397px; z-index: 204; -webkit-transform: scale(2); -ms-transform: scale(2); transform: scale(2);">'+name+'</span></div></div>');
+							$("#people").append('<div class="wrapperPeople" id="'+me_id+'" style="-webkit-transform: scale(1, 1); -ms-transform: scale(1, 1); transform: scale(1, 1); position: relative;"><div id="'+me_id+'_sub" class="person" style="position: absolute; top: 0; left: 0; z-index: 203; -webkit-transform: scale(0.54); -ms-transform: scale(0.54); transform: scale(0.54);">'+avatar+'<img style="position: absolute; top: 178px; left: -70px; z-index: 202;" width="200%" src="svg/human/humanBody/cloud.svg"/><span style="position: absolute; top: 397px; z-index: 204; -webkit-transform: scale(2); -ms-transform: scale(2); transform: scale(2);">'+name+'</span></div></div>');
 							$("#saturn").hide();
 						});
 					}
