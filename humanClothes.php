fc3bff1ec1f70da69632d372113c333b690d4226
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-10-07 00:04:15 -0400

    remove ending span

--- humanClothes.php
+++ humanClothes.php
@@ -297,7 +297,7 @@
 		name = document.getElementById("avatarName").value;
 		avatar = document.getElementById("relativeContainer").innerHTML;
 		$("#relativeContainer").css({'margin':'0px', 'margin-left':'0px', 'width':'400px'});
-		var sprite = '<div class="sprite" id="sprite" style="-ms-transform: scale(0.4); -webkit-transform: scale(0.4); transform: scale(0.4);">'+avatar+'</div><img style="position: absolute; top: 38px; left: -40px; z-index: 202;" width="200%" src="svg/human/humanBody/cloud.svg"/><span style="position: absolute; top: 397px; z-index: 204; -webkit-transform: scale(2); -ms-transform: scale(2); transform: scale(2);">'+name+'</span></span>';
+		var sprite = '<div class="sprite" id="sprite" style="-ms-transform: scale(0.4); -webkit-transform: scale(0.4); transform: scale(0.4);">'+avatar+'<img style="position: absolute; top: 38px; left: -40px; z-index: 202;" width="200%" src="svg/human/humanBody/cloud.svg"/><br>'+name+'</div>';
 		document.body.innerHTML = sprite;
 		$(".blinking .blink").css({'visibility':'visible'});
 		/*var imgs = document.getElementsByTagName("img");
